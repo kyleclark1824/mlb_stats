71be98ee6ff8d91934a413b052464ddc0d11e4e8
kyleclark1824 <kyle.clark1824@gmail.com>
2025-10-29 22:08:51 -0700

    more key fun

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../../public/supabase-config';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './supabase-config';
 
 export const supabase = createClient(
   SUPABASE_URL,
